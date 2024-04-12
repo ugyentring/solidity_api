5fc5aee55d36fc2b151dfc2f8e16e67e39d2f400
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-12 18:31:58 +0600

    hh

--- index.js
+++ index.js
@@ -1,89 +1,88 @@
-const ethers = require('ethers');
-require('dotenv').config();
+const ethers = require("ethers");
+require("dotenv").config();
 const sepolia_url = process.env.sepolia_url;
 const private_key = process.env.private_key;
 const contractAddress = process.env.contract_address;
 
 const provider = new ethers.providers.JsonRpcProvider(sepolia_url);
 const signer = new ethers.Wallet(private_key, provider);
-const {abi} = require("./artifacts/contracts/contractApi.sol/contractApi.json");
+const {
+  abi,
+} = require("./artifacts/contracts/contractApi.sol/contractApi.json");
 const contractInstance = new ethers.Contract(contractAddress, abi, signer);
 
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get('/products/:id', async(req, res) => {   //http://localhost:3000/products/1
-    try {
-        const id = req.params.id;
-        const product = await contractInstance.getProduct(id);
-        let prod = []
-        prod[0] = product[0];
-        prod[1] = parseInt(product[1]);
-        prod[2] = parseInt(product[2]);
-        res.send(prod);
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
+app.get("/products/:id", async (req, res) => {
+  //http://localhost:3000/products/1
+  try {
+    const id = req.params.id;
+    const product = await contractInstance.getProduct(id);
+    let prod = [];
+    prod[0] = product[0];
+    prod[1] = parseInt(product[1]);
+    prod[2] = parseInt(product[2]);
+    res.send(prod);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.get('/products/', async(req, res) => {   //http://localhost:3000/products/
-    try {
-        const allProducts = await contractInstance.getAllProducts();
-        const products = allProducts.map(product => ({
-            id : parseInt(product.id),
-            name: product.name,
-            price: parseInt(product.price),
-            quantity: parseInt(product.quantity)
-        }))
-        console.log(products)
-        res.send(products);
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
+app.get("/products/", async (req, res) => {
+  //http://localhost:3000/products/
+  try {
+    const allProducts = await contractInstance.getAllProducts();
+    const products = allProducts.map((product) => ({
+      id: parseInt(product.id),
+      name: product.name,
+      price: parseInt(product.price),
+      quantity: parseInt(product.quantity),
+    }));
+    console.log(products);
+    res.send(products);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-
-app.post('/products', async(req, res) => {
-    try {
-        const {id, name, price, quantity} = req.body;
-        const tx = await contractInstance.setProduct(id, name, price, quantity);
-        await tx.wait();
-        res.json({success: true})
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
+app.post("/products", async (req, res) => {
+  try {
+    const { id, name, price, quantity } = req.body;
+    const tx = await contractInstance.setProduct(id, name, price, quantity);
+    await tx.wait();
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.put('/products/:id', async (req, res) => {   //http://localhost:3000/products/1
-    try {
-        const id = req.params.id;
-        const {name, price, quantity} = req.body;
-        const tx = await contractInstance.updateProduct(id, name, price, quantity);
-        await tx.wait();
-        res.json({success: true})
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
+app.put("/products/:id", async (req, res) => {
+  //http://localhost:3000/products/1
+  try {
+    const id = req.params.id;
+    const { name, price, quantity } = req.body;
+    const tx = await contractInstance.updateProduct(id, name, price, quantity);
+    await tx.wait();
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.delete('/products/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const tx = await contractInstance.deleteProduct(id);
-        await tx.wait();
-        res.json({success: true})
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tx = await contractInstance.deleteProduct(id);
+    await tx.wait();
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log("API server is listening on port 3000")
-})
+  console.log("API server is listening on port 3000");
+});
